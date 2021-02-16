038de85e2d4adc62d7c052b9b5713233871e338f
Peter Hyman <pete@peterhyman.com>
2021-02-16 09:07:16 -0600

    From master:71bbbe9 Graciously handle signals when in a detached state

--- src/main.c
+++ src/main.c
@@ -165,6 +165,8 @@ static void sighandler(int sig __UNUSED__)
 {
 	signal(sig, SIG_IGN);
 	signal(SIGTERM, SIG_IGN);
+	signal(SIGTTIN, SIG_IGN);
+	signal(SIGTTOU, SIG_IGN);
 	print_err("Interrupted\n");
 	fatal_exit(&local_control);
 }
@@ -770,6 +772,7 @@ recursion:
 
 		if (STDIN)
 			control->inFILE = stdin;
+
 		/* Implement signal handler only once flags are set */
 		sigemptyset(&handler.sa_mask);
 		handler.sa_flags = 0;
