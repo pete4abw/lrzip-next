f55802a601b8d52dc027f385145843455feab3d3
Peter Hyman <pete@peterhyman.com>
2021-07-13 06:22:37 -0500

    Correctly set and clear control->hash when encrypting. NOTE: Past lrzip-next encrypted files may be broken.

--- src/lrzip.c
+++ src/lrzip.c
@@ -676,7 +676,7 @@ retry_pass:
 static void release_hashes(rzip_control *control)
 {
 	memset(control->salt_pass, 0, PASS_LEN);
-	memset(control->hash, 0, SALT_LEN);
+	memset(control->hash, 0, HASH_LEN);
 	munlock(control->salt_pass, PASS_LEN);
 	munlock(control->hash, HASH_LEN);
 	dealloc(control->salt_pass);
