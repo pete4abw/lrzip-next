0d5da28b7709e2e00fc7fe6ac7d7a5e530987412
Peter Hyman <pete@peterhyman.com>
2022-05-03 07:53:19 -0500

    BUGFIX: Issue #74. Don't try and show compression stats when STDOUT used.

--- src/rzip.c
+++ src/rzip.c
@@ -1260,9 +1260,15 @@ retry:
 
 	if (!STDIN)
 		print_progress("%s - ", control->infile);
-	print_progress("Compression Ratio: %.3f. bpb: %.3f. Average Compression Speed: %6.3fMB/s.\n",
-		       1.0 * s.st_size / s2.st_size,
-		       8.0 * s2.st_size / s.st_size, chunkmbs);
+	else
+		print_progress("STDIN - ");
+
+	if (!STDOUT)
+		print_progress("Compression Ratio: %.3f. bpb: %.3f. Average Compression Speed: %6.3fMB/s.\n",
+				1.0 * s.st_size / s2.st_size,
+				8.0 * s2.st_size / s.st_size, chunkmbs);
+	else
+		print_progress("Cannot compute compression ratio with STDOUT\n");
 
 	clear_sslist(st);
 	gcry_md_close(control->hash_handle);
