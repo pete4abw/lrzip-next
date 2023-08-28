7d4c005a26693f73d54bc549be38dddce42dc7b9
Peter Hyman <pete@peterhyman.com>
2023-08-28 07:46:45 -0500

    Updates to speedtest to allow specific methods.

--- test/speedtest.sh
+++ test/speedtest.sh
@@ -2,12 +2,18 @@
 # lrzip-next speed test
 # if running as root, uncomment drop_caches line
 usage() {
-echo "LRZIP Speed Test"
-echo "usage: $0 filename"
+echo "LRZIP-NEXT Speed Test"
+echo "usage: $0 filename [compression method(s)]"
+echo "for multiple methods, be sure to quote. i.e. \"bzip2 lzo gzip\" etc..."
 exit 1
 }
 
 [ -z $1 ] && usage
+if [ -z $2 ]; then
+	methods="bzip2 bzip3 gzip lzo lzma zpaq zstd"
+else
+	methods="$2"
+fi
 
 export LRZIP=NOCONFIG
 
@@ -15,19 +21,24 @@ export LRZIP=NOCONFIG
 OUTPUTDIR=$PWD
 TESTFILE=testfile.txt
 BASENAME=$(basename $1)
-
-echo "Compression/Decompression test for file $1"
-echo "Compression/Decompression test for file $1" >$TESTFILE
+UID=$(id -u)
+echo "Compression/Decompression test for file $1 using method(s): $methods"
+echo "Compression/Decompression test for file $1 using method(s): $methods" >$TESTFILE
 ls -l $1 1>>$TESTFILE
+
+[ $UID -eq 0 ] && echo "Running as root user."
 
 for i in 1 2 3 4 5 6 7 8 9
 do
-	for j in bzip2 bzip3 gzip lzo lzma zpaq zstd
+	for j in $methods
 	do
 		sync
 		sleep 1
-#		echo 3 >/proc/sys/vm/drop_caches
-#		sleep 1
+		# root user?
+		if [ $UID -eq 0 ]; then
+			echo 3 >/proc/sys/vm/drop_caches
+			sleep 1
+		fi
 		OUTPUT=$BASENAME.L$i.$j.lrz
 		echo "Testing $j at Level $i"
 		echo -n "Testing $j at Level $i : " >>$TESTFILE
