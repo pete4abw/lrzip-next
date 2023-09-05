3905a4edfa551e0cb173a9e9f05491288676e97f
Peter Hyman <pete@peterhyman.com>
2023-09-05 12:27:44 -0500

    Major updates to speedtest.sh program
    Will output to a CSV file with all timing and stats
    data for compression and decompression for selected
    methods and levels. See usage message for info.

--- test/speedtest.sh
+++ test/speedtest.sh
@@ -2,35 +2,63 @@
 # lrzip-next speed test
 # if running as root, uncomment drop_caches line
 usage() {
-echo "LRZIP-NEXT Speed Test"
-echo "usage: $0 filename [compression method(s)]"
-echo "for multiple methods, be sure to quote. i.e. \"bzip2 lzo gzip\" etc..."
-exit 1
+	echo "LRZIP-NEXT Speed Testi\
+usage: $0 filename [compression method(s)] [levels]\n\
+methods may be one or more of [no-compress, bzip2, bzip3, gzip, lzo, lzma, zpaq, zstd]\n\
+for multiple methods, be sure to quote. i.e. \"bzip2 lzo gzip\" etc...\n\
+levels may be one or more of [1, 2, 3, 4, 5, 6, 7, 8, 9] in any order.\n\
+for levels, must follow methods and levels must be quoted. i.e. \"1 2 3\" etc...\n\
+if not methods or levels specified, all methods and levels will be tested.\n\
+Output file will be $TESTFILE, a comma-delimited file."
+	exit 1
 }
 
+die() {
+	echo "Error: $1...Aborting"
+	exit 1
+}
+
+TESTFILE=testfile.csv
+
 [ -z $1 ] && usage
-if [ -z $2 ]; then
-	methods="bzip2 bzip3 gzip lzo lzma zpaq zstd"
+if [ -z "$2" ]; then
+	methods="no-compress bzip2 bzip3 gzip lzo lzma zpaq zstd"
 else
 	methods="$2"
+fi
+if [ -z "$3" ]; then
+	levels="1 2 3 4 5 6 7 8 9"
+else
+	levels="$3"
 fi
 
 export LRZIP=NOCONFIG
 
 # Customize as needed
+INPUT=$1
 OUTPUTDIR=$PWD
-TESTFILE=testfile.txt
-BASENAME=$(basename $1)
+BASENAME=$(basename $INPUT)
 UID=$(id -u)
-echo "Compression/Decompression test for file $1 using method(s): $methods"
-echo "Compression/Decompression test for file $1 using method(s): $methods" >$TESTFILE
-ls -l $1 1>>$TESTFILE
+# use binary versions of time and stat
+TIME=$(which time)
+[ $? -ne 0 ] && die "time program not found" 
+STAT=$(which stat)
+[ $? -ne 0 ] && die "stat program not found"
+INPUTSIZE=$( $STAT --print "%s" $INPUT )
+[ $? -ne 0 ] && die "Input file $INPUT not found"
+echo "Compression/Decompression test for file $INPUT, $INPUTSIZE  using method(s): $methods"
 
 [ $UID -eq 0 ] && echo "Running as root user."
 
-for i in 1 2 3 4 5 6 7 8 9
+# Write headers
+
+echo "Method, Level, Input File, Input Size, Compress Time, \
+Compressed File, Compressed Size, Decompress Time, Compression Ratio, \
+Bits per Byte, Compression MB/s" >$TESTFILE
+
+for LEVEL in $levels
 do
-	for j in $methods
+	for METHOD in $methods
 	do
 		sync
 		sleep 1
@@ -39,26 +67,21 @@ do
 			echo 3 >/proc/sys/vm/drop_caches
 			sleep 1
 		fi
-		OUTPUT=$BASENAME.L$i.$j.lrz
-		echo "Testing $j at Level $i"
-		echo -n "Testing $j at Level $i : " >>$TESTFILE
-		lrzip-next -qf --$j -L$i -o $OUTPUTDIR/$OUTPUT $1 1>>$TESTFILE
-		if [ $? -ne 0 ] ; then
-			echo "An error occured during compression!"
-			exit 1
-		fi
-		echo "Decompressing $OUTPUT"
-		echo -n "Decompressing $OUTPUT : " >>$TESTFILE
-		lrzip-next -qt $OUTPUTDIR/$OUTPUT 1>>$TESTFILE
-		if [ $? -ne 0 ] ; then
-			echo "An error occured during decompression!"
-			exit 1
-		fi
+		OUTPUT=$BASENAME.L$LEVEL.$METHOD.lrz
+		echo -n "Testing $INPUT using $METHOD at Level $LEVEL: "
+		COMPRESSTIME=$( { $TIME --format "%e" lrzip-next -Qf --$METHOD -L$LEVEL -o $OUTPUTDIR/$OUTPUT $INPUT; } 2>&1 )
+		[ $? -ne 0 ] && die "An error occured during compression!"
+		OUTPUTSIZE=$( $STAT --print "%s" $OUTPUT )
+		echo "Compression Time: $COMPRESSTIME, Compressed Size: $OUTPUTSIZE"
+		echo -n "Decompressing $OUTPUT: "
+		DECOMPRESSTIME=$( { $TIME --format "%e" lrzip-next -Qt $OUTPUTDIR/$OUTPUT; } 2>&1 )
+		[ $? -ne 0 ] && die "An error occured during decompression!"
+		echo "Decompression Time: $DECOMPRESSTIME"
+		COMPRESSIONRATIO=$(echo "scale=3; $INPUTSIZE/$OUTPUTSIZE" | bc -l)
+		BITSPERBYTE=$(echo "scale=3; 8*$OUTPUTSIZE/$INPUTSIZE" | bc -l)
+		COMPRESSIONMBS=$(echo "scale=3; $INPUTSIZE/(1048576*$COMPRESSTIME)" | bc -l)
+		echo "$METHOD, $LEVEL, $INPUT, $INPUTSIZE, $COMPRESSTIME, $OUTPUT, $OUTPUTSIZE, $DECOMPRESSTIME, $COMPRESSIONRATIO, $BITSPERBYTE, $COMPRESSIONMBS" >> $TESTFILE
 	done
 done
-
-echo "Test completed. Here are the files..." >>$TESTFILE
-(cd $OUTPUTDIR
-ls -l $BASENAME*.lrz 1>>$TESTFILE
-)
+echo "Results stored in: $TESTFILE"
 exit 0
